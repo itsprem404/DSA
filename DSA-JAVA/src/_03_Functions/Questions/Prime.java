4ce74d00cfc2e1fba3479eba425813c80ae4ec18
Prem404 <ppremkkumar010@gmail.com>
2026-08-05 23:03:13 +0530

    DSA

--- DSA-JAVA/src/_03_Functions/Questions/Prime.java
+++ DSA-JAVA/src/_03_Functions/Questions/Prime.java
@@ -17,7 +17,7 @@ public class Prime {
             return "is Prime";
         }
 
-        for (int i = 3; i * i <= num; i += 2) {
+        for (int i = 3; i * i <= num; i +=2) {
             if (num % i == 0) {
                 return "Not Prime";
             }else return "Prime";
