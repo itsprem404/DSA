ba5ff302cdfa70ae385920b0e5e4f8851b8c1924
Prem404 <ppremkkumar010@gmail.com>
2026-08-05 22:05:39 +0530

    DSA

---
+++ DSA-JAVA/src/_03_Functions/Questions/Max_Min.java
@@ -0,0 +1,37 @@
+package _03_Functions.Questions;
+import java.util.*;
+
+/*Q. Define two methods to print the maximum and the minimum number respectively
+among three numbers entered by the user.*/
+
+public class Max_Min {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 3 numbers: ");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int num3 = in.nextInt();
+
+        int minimum = Min(num1, num2, num3);
+        System.out.println("Minimum: " + minimum);
+
+        int maximum = Max(num1, num2, num3);
+        System.out.println("Maximum: " + maximum);
+    }
+
+    static int Min(int num1, int num2, int num3) {
+        if (num1 <= num2 && num1 <= num3) {
+            return num1;
+        } else if (num2 <= num1 && num2<=num3) {
+            return num2;
+        }else return num3;
+    }
+
+        static int Max ( int num1, int num2, int num3){
+            if (num1 >= num2 && num1 >= num3) {
+                return num1;
+            } else if (num2 >= num1 && num2 >= num3) {
+                return num2;
+            } else return num3;
+        }
+}
