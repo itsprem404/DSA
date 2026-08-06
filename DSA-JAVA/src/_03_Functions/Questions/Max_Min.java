62052edb96e85ca639a65db22b688121205b1c78
Prem404 <ppremkkumar010@gmail.com>
2026-08-06 21:58:39 +0530

    DSA

--- DSA-JAVA/src/_03_Functions/Questions/Max_Min.java
+++ DSA-JAVA/src/_03_Functions/Questions/Max_Min.java
@@ -11,8 +11,8 @@ public class Max_Min {
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         int num3 = in.nextInt();
-        System.out.println("Minimum: " + Min(num1, num2, num3));
-        System.out.println("Maximum: " + Max(num1, num2, num3));
+        System.out.println("Minimum number: " + Min(num1, num2, num3));
+        System.out.println("Maximum number: " + Max(num1, num2, num3));
     }
 
     static int Min(int num1, int num2, int num3) {
