f972967078782c1bb220137a3a69ed56199fe222
Prem404 <ppremkkumar010@gmail.com>
2026-08-07 08:52:11 +0530

    DSA

---
+++ DSA-JAVA/src/_000_TutorialQuestions/Tutorial3/Employee.java
@@ -0,0 +1,33 @@
+package TutorialQuestions.Tutorial3;
+
+import java.util.Scanner;
+
+public class Employee {
+    int employeeID;
+    String employeeName;
+    double salary;
+
+    public Employee(int employeeID, String employeeName, double salary) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Employee ID : " + employeeID);
+        System.out.println("Employee Name : " + employeeName);
+        System.out.println("Salary : " + (int)salary);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int id = sc.nextInt();
+        sc.nextLine();
+        String name = sc.nextLine();
+        double sal = sc.nextDouble();
+
+        Employee emp = new Employee(id, name, sal);
+        emp.displayDetails();
+    }
+}
